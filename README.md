9056f81c8eeba14ad6051448eed19f11602fc5bf
parnavh <parnav100sach@gmail.com>
2024-03-24 11:53:07 +0530

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Event Management System
+
+## WIP
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import { env } from "./env";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
