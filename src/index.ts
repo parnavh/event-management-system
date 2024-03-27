ec6d6f72cc009482fdf85b3e6dfc29a207633713
Parnav Harinathan <45985534+parnavh@users.noreply.github.com>
2024-03-27 14:51:19 +0000

    chore: added curl endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import "@/env";
+import { env } from "@/env";
 import eventsRouter from "@/routes/events";
 import errorHandler from "@/middleware";
 import swaggerUI from "swagger-ui-express";
@@ -15,9 +15,15 @@ app.use("/docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use("/events", eventsRouter);
 
-app.use((_, res, __) => {
+app.get("/curl", (_, res) => {
+  res.send(`View interactive demo at ${env.SERVER_URL}\n\nOr use the following curl command:\n\ncurl -X 'GET' \
+  '${env.SERVER_URL}/events/find?date=2024-03-15&latitude=40.7128&longitude=-74.0060&page=1&pageSize=10' \
+  -H 'accept: application/json'`);
+});
+
+app.use((_, res) => {
   res.redirect("/docs");
-})
+});
 
 app.use(errorHandler);
 
